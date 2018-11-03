c506b9242363f9760842dcc21be505010513e923
Cichorium <38894874+Cichorium@users.noreply.github.com>
2018-11-03 19:27:46 +0100

    Created node module jsonoptpack.

--- jsonoptpack.js
+++ jsonoptpack.js
@@ -189,27 +189,6 @@ var jsonoptpack = function()
       return null // TODO
     }
   }
-}
+}()
 
-/*
-[{ "a" : "string0", "b": 40},{ "a" : "string1", "b": 41},{"c": true, "d":45, "e": "string2"},{ "a" : "string2", "b": 42}]
-"2|2|3|21012|a|b|c|d|e|
-a4|0|1|0|2|1|1| or a4|010|2|1|1| or a4|010|211|
-string0|string1|string2|40|41|42|1|45|@2"
-
-  2|2|3|21012|a|b|c|d|e|a4|010|211|string0|string1|string2|40|41|42|1|45|@2
-*/
-
-var test = [
-  {"a": "string0", "b": 40},
-  {"a": "string1", "b": 41},
-  {"c": true, "d": 45, "e": "string2"},
-  {"a": "string2", "b": 42}
-]
-
-console.log(JSON.test)
-
-var jp = new jsonoptpack
-var px = jp.pack(test)
-console.log(px)
-console.log(jp.unpack(px))
+if (typeof module != "undefined" && module.exports) { module.exports = jsonoptpack }
---
+++ test.js
@@ -0,0 +1,23 @@
+var jsonoptpack = require("./jsonoptpack");
+
+/*
+[{ "a" : "string0", "b": 40},{ "a" : "string1", "b": 41},{"c": true, "d":45, "e": "string2"},{ "a" : "string2", "b": 42}]
+"2|2|3|21012|a|b|c|d|e|
+a4|0|1|0|2|1|1| or a4|010|2|1|1| or a4|010|211|
+string0|string1|string2|40|41|42|1|45|@2"
+
+  2|2|3|21012|a|b|c|d|e|a4|010|211|string0|string1|string2|40|41|42|1|45|@2
+*/
+
+var test = [
+  {"a": "string0", "b": 40},
+  {"a": "string1", "b": 41},
+  {"c": true, "d": 45, "e": "string2"},
+  {"a": "string2", "b": 42}
+]
+
+console.log(test)
+
+var px = jsonoptpack.pack(test)
+console.log(px)
+console.log(jsonoptpack.unpack(px))
