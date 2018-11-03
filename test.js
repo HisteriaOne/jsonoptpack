9517d472706fc7de7b7d005db0accc30da9356fd
Cichorium <42416399+Cichorium@users.noreply.github.com>
2018-11-03 17:24:25 +0100

    Added pack/unpack schema.

--- test.js
+++ test.js
@@ -16,8 +16,11 @@ jsonoptpack.prototype.pack = function(data)
 {
   var out = []
 
-  this.appendObjects(out, data)
-  this.appendSchema(out, data)
+  var objects = {}
+  this.appendObjects(out, objects, data)
+  console.log(objects)
+
+  this.appendSchema(out, objects, data)
   this.appendData(out, data)
 
   return out.join("|")
@@ -27,7 +30,8 @@ jsonoptpack.prototype.unpack = function(str)
 {
   if (typeof str !== "string") { return str }
 
-  var a = str.split("|") // TODO: strings can contain symbol "|", we must handle it too
+  // TODO: strings can contain symbol "|", it means we must implement own splitter with handling "\|" and "\\"
+  var a = str.split("|")
 
   var objects = []
   var i = this.unpackObjects(a, 0, objects)
@@ -47,11 +51,9 @@ jsonoptpack.prototype.appendData = function(out, data)
   // TODO
 }
 
-jsonoptpack.prototype.appendObjects = function(out, data)
+jsonoptpack.prototype.appendObjects = function(out, objects, data)
 {
   if (data === null) { return }
-
-  var objects = {}
 
   this.findObjects(objects, data)
 
@@ -80,9 +82,80 @@ jsonoptpack.prototype.appendObjects = function(out, data)
   out[f] = dt.join("")
 }
 
-jsonoptpack.prototype.appendSchema = function(out, data)
+jsonoptpack.prototype.appendSchema = function(out, objects, data)
 {
-  // TODO
+  if (data === null || typeof data !== "object") { return }
+
+  if (!Array.isArray(data)) {
+    out.push(objectkeys[this.getObjectKey(data)])
+    return
+  }
+
+  var objectkeys = {}
+  var ol = 0
+  for (var k in objects) { objectkeys[k] = ol++ }
+  console.log(objectkeys)
+
+  out.push("a" + data.length)
+
+  var prev = null
+  var j = 0
+  var sizes = []
+  var ola = []
+  var k
+  for (var i in data) {
+    var v = data[i]
+    if (typeof v !== "object") {
+      // TODO: next step: check string, numbers, arrays, etc.
+      j = 0
+      continue
+    }
+
+    k = this.getObjectKey(v)
+    if (prev === null) {
+      prev = k
+      j = 1
+      continue
+    }
+
+    if (k === prev) {
+      j++
+      continue
+    }
+
+    sizes.push(j)
+    j = 1
+    ola.push(objectkeys[prev])
+    prev = k
+  }
+  ola.push(objectkeys[k])
+
+  out.push(ola.join(ol <= 9 ? "" : "|"))
+
+  var sz = []
+  for (var i in sizes) { sz.push(sizes[i]) }
+  sz.push(j)
+  out.push(sz.join(data.length <= 9 ? "" : "|"))
+}
+
+jsonoptpack.prototype.findObjects = function(objects, data)
+{
+  if (data === null || typeof data !== "object") { return }
+
+  if (Array.isArray(data)) {
+    for (var i in data) { this.findObjects(objects, data[i]) }
+    return
+  }
+
+  var i = 0
+  var m = {}
+  for (var k in data) {
+    i++
+    m[k] = this.getDatatype(data[k])
+  }
+  objects[this.getObjectKey(data)] = [i, m]
+
+  for (var k in data) { this.findObjects(objects, data[k]) }
 }
 
 jsonoptpack.prototype.getDatatype = function(value)
@@ -96,28 +169,11 @@ jsonoptpack.prototype.getDatatype = function(value)
   if (typeof value === "object")  { return 5 }
 }
 
-jsonoptpack.prototype.findObjects = function(objects, data)
+jsonoptpack.prototype.getObjectKey = function(object)
 {
-  if (data === null || typeof data !== "object") { return }
-
-  if (Array.isArray(data)) {
-    for (var i in data) { this.findObjects(objects, data[i]) }
-    return
-  }
-
   var a = []
-  var m = {}
-  var i = 0
-  for (var k in data) {
-    var t = this.getDatatype(data[k])
-    a.push(k,t)
-    m[k] = t
-    i++
-  }
-
-  objects[a.join("|")] = [i, m]
-
-  for (var k in data) { this.findObjects(objects, data[k]) }
+  for (var k in object) { a.push(k, this.getDatatype(object[k])) }
+  return a.join("|")
 }
 
 jsonoptpack.prototype.unpackObjects = function(a, index, objects)
@@ -160,10 +216,10 @@ jsonoptpack.prototype.unpackObjects = function(a, index, objects)
 */
 
 var test = [
-  {"a" : "string0", "b": 40},
-  {"a" : "string1", "b": 41},
-  {"c": true, "d":45, "e": "string2"},
-  {"a" : "string2", "b": 42}
+  {"a": "string0", "b": 40},
+  {"a": "string1", "b": 41},
+  {"c": true, "d": 45, "e": "string2"},
+  {"a": "string2", "b": 42}
 ]
 
 console.log(test)
